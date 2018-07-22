35ad697bb3886058380e6708c71e2dacd274a732
Marcos Monteiro <marcosmmb90@gmail.com>
2018-07-22 13:38:58 -0300

    various improvements and config json added

--- nano_bridge.py
+++ nano_bridge.py
@@ -4,33 +4,47 @@ from flask_jsonpify import jsonify
 import requests
 import json
 
-# independent function that create a bridge between a nano node and a public route for RESTful access
-
-HOST = "" # machine ip
-PORT = 6060
-URI = "http://[::1]:7076"
-
-def sendRpcRequest(data):
-    data = json.dumps(data)
-
-    response = requests.post(URI, data=data)
-    if not response.ok:
-        return None
-    resp_dict = json.loads(response.text)
-    return resp_dict
+# independent code that create a bridge between a nano node and a public route for RESTful access
 
 class Bridge(Resource):
     def post(self):
         json_dict = request.get_json()
         return jsonify(sendRpcRequest(json_dict))
 
-def main():
-    app = Flask(__name__)
-    api = Api(app)
+def config(*args):
+    with open('config.json') as json_data_file:
+        configuration = json.load(json_data_file)
 
-    api.add_resource(Bridge, "/")
+    config_dict = configuration
+    for argument in args:
+        config_dict = config_dict[argument]
+    return config_dict
+
+def defaultHost(api = config("ip-api")):
+    try:
+        host = requests.get(api, timeout = float(config("timeout")) ).text
+    except:
+        host = config("host")
+    finally:
+        return host
+
+def sendRpcRequest(data):
+    data = json.dumps(data)
+
+    response = requests.post(config("uri"), data = data, timeout = float(config("timeout")) )
+    if not response.ok:
+        return None
+    resp_dict = json.loads(response.text)
+    return resp_dict
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Bridge, "/")
     
-    if __name__ == "__main__":
-        app.run(host=HOST, port=PORT)
+if __name__ == "__main__":
+    try:
+        app.run(host = defaultHost(), port = config("port"))
+    except:
+        app.run(host = config("host"), port = config("port"))
 
-main()
